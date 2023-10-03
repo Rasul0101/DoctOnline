77e3c51a587238e382b7b872f50775bbd0f2f01e
Rasul0101 <rasul.aghamammadli@gmail.com>
2023-10-03 21:00:23 +0400

    my first commit

---
+++ js/dropdown.js
@@ -0,0 +1,5 @@
+// dropdown
+let subMenu = document.getElementById("subMenu");
+function toggleMenu() {
+  subMenu.classList.toggle("open-menu");
+}
---
+++ js/swiper.js
@@ -0,0 +1,69 @@
+// First Info Swiper
+const swiper = new Swiper(".first", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".first-next",
+    prevEl: ".first-prev",
+  },
+
+  // Default parameters
+  slidesPerView: 2,
+  spaceBetween: 32,
+});
+
+// Famous Swiper
+const swiper2 = new Swiper(".second", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".second-next",
+    prevEl: ".second-prev",
+  },
+
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 32,
+});
+
+// New Swiper
+const swiper3 = new Swiper(".third", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".third-next",
+    prevEl: ".third-prev",
+  },
+
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 32,
+});
+
+// Suggestion Swiper
+const swiper4 = new Swiper(".fourth", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".fourth-next",
+    prevEl: ".fourth-prev",
+  },
+
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 32,
+});
+
+
